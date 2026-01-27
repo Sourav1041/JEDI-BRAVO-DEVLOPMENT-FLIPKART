7aea30bc977e3e7edfb39c537d846809f0d709fb
Sourav <agrawalsourav87@example.com>
2026-01-27 14:44:35 +0530

    final project structure

--- JEDI_BRAVO_FLIPFIT_PROJECT_DEVELOPMENT_POS_JDBC/JEDI_BRAVO_FLIPFIT_PROJECT_DEVELOPMENT_POS_COLLECTION/src/com/flipfit/enums/BookingStatus.java
+++
@@ -1,7 +0,0 @@
-package com.flipfit.enums;
-
-public enum BookingStatus {
-	    CONFIRMED,
-	    WAITLIST,
-	    CANCELLED
-}
--- JEDI_BRAVO_FLIPFIT_PROJECT_DEVELOPMENT_POS_JDBC/JEDI_BRAVO_FLIPFIT_PROJECT_DEVELOPMENT_POS_COLLECTION/src/com/flipfit/enums/Role.java
+++
@@ -1,7 +0,0 @@
-package com.flipfit.enums;
-
-public enum Role {
-    ADMIN,
-    GYM_OWNER,
-    CUSTOMER
-}
